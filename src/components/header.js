672db814582f0bca967f1bd0777789ca3542c767
Pedro Seneriz <pedroseneriz01@gmail.com>
2023-12-09 14:39:05 -0300

    cambio el login al componente login

--- src/components/header.js
+++ src/components/header.js
@@ -1,87 +1,24 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Header.css';
-import { Button, Nav, Row, Col, Form, Card, Navbar, Container } from 'react-bootstrap';
-import { Formik, ErrorMessage } from 'formik';
-import * as Yup from "yup";
-
-const formSchema = Yup.object().shape({
-    correo: Yup.string()
-        .required("Campo requerido"),
-    pass: Yup.string()
-        .required("Campo requerido"),
-});
+import { Button, Nav, Navbar, Container } from 'react-bootstrap';
 
 const URL = process.env.REACT_APP_BACKEND_CONNECTION;
 const userVacio = { legajo: '', correo: '', rol: '', pass: '' };
 const Header = () => {
     const usuarioAux = localStorage.getItem('usuario');
     const [usuario, setUsuario] = useState(userVacio);
-    const [mostrarLogin, setMostrarLogin] = useState(false);
-
-    const [initialValues, setInitialValues] = useState({ correo: '', pass: '' });
-
 
     useEffect(() => {
         if (localStorage.getItem("usuario"))
             setUsuario(JSON.parse(localStorage.getItem("usuario")));
     }, []);
 
-    // useEffect(() => {
-    //     if(localStorage.getItem('usuario')){
-    //         setUsuario(JSON.parse(localStorage.getItem('usuario')));
-    //     }
-    //     else setUsuario({legajo:'',nombreUsuario:'', rol:'', pass: ''});
-    // },[usuarioLogueado]);
 
     function cerrarSesion() {
         localStorage.clear();
         setUsuario(userVacio);
         window.location.assign("/");
     }
-
-    const Ingresar = (values) => {
-        const usuarioAux = {
-            correo: values.correo,
-            clave: values.pass,
-        };
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(usuarioAux),
-        };
-
-        console.log("Ingresar");
-        fetch(URL + 'api/Usuarios/Login', options)
-            .then((res) => res.ok ? res.json() : Promise.reject(res))
-            .then((data) => {
-                console.log(data);
-                setUsuario(
-                    {
-                        legajo: data.id,
-                        correo: data.titulo,
-                        rol: (data.tipoUsuario == 1) ? "Alumno" : (data.tipoUsuario == 2) ? "Profesor" : "Administrativo"
-                    });
-
-                localStorage.setItem("usuario", JSON.stringify(data));
-            })
-            .catch((err) => {
-                console.error(err);
-            })
-            .finally(() => {
-                window.location.assign("/");
-            });
-        //let usuarioAux = usuarios.find(element => element.nombreUsuario === values.nombreUsuario && element.pass === values.pass );
-        // if(usuarioAux!= undefined){
-        //     localStorage.setItem("usuario",  JSON.stringify(usuarioAux));
-        //     window.location.assign("/");
-        // }
-        // else{
-        //     window.location.assign("/");
-        // }
-    };
 
     return (
         <>
@@ -125,7 +62,9 @@ const Header = () => {
                                         <label className='p-1' style={{ color: '#FAFAFA' }} >{usuario.correo} </label>
                                     </div>
                                     :
-                                    <Button onClick={() => { setMostrarLogin(true); }}> Iniciar sesión</Button>
+                                    <Button >
+                                        <Nav.Link style={{ color: 'white' }} href="/login">Iniciar sesion</Nav.Link>
+                                    </Button>
                             }
                         </Nav>
 
@@ -134,81 +73,7 @@ const Header = () => {
                 </Container>
             </Navbar>
 
-            {mostrarLogin &&
-                <Row className="justify-content-center">
-                    <Col>
-                        <Card style={{ padding: 10, color: "black" }} >
-                            <Formik
-                                initialValues={initialValues}
-                                validationSchema={formSchema}
-                                onSubmit={Ingresar}>
-                                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                                    <>
-                                        <Row>
-                                            <Col>
-                                                <img style={{ height: "75px" }} alt="Logo PPSII Académico" />
-                                            </Col>
-                                        </Row>
-                                        <Row >
-                                            <Col >
-                                                <Form.Group controlId="correo">
-                                                    <Form.Label style={{ ...{ color: "black" } }}>E-mail: </Form.Label>
-                                                    <Form.Control
-                                                        type="text"
-                                                        name="correo"
-                                                        autoComplete='off'
-                                                        onChange={handleChange}
-                                                        value={values.correo}
-                                                    />
-                                                </Form.Group>
-                                                <ErrorMessage
-                                                    name='correo'
-                                                    component='div'
-                                                    className='field-error text-danger'
-                                                />
-                                            </Col>
-                                        </Row>
-                                        <Row >
-                                            <Col >
-                                                <Form.Group controlId="pass">
-                                                    <Form.Label style={{ ...{ color: "black" } }}>Contraseña: </Form.Label>
-                                                    <Form.Control
-                                                        type="password"
-                                                        name="pass"
-                                                        autoComplete='off'
-                                                        onChange={handleChange}
-                                                        value={values.pass}
-                                                    />
-                                                </Form.Group>
-                                                <ErrorMessage
-                                                    name='pass'
-                                                    component='div'
-                                                    className='field-error text-danger'
-                                                />
-                                            </Col>
-                                        </Row>
 
-                                        <Row className="justify-content-center">
-                                            <Col >
-                                                <Button
-                                                    variant="primary"
-                                                    onClick={handleSubmit}
-                                                    style={{ width: "100%", background: "#FFFFFF", borderColor: "#009AAE", color: "#009AAE" }}
-                                                >
-                                                    Ingresar
-                                                </Button>
-                                            </Col>
-                                        </Row>
-                                        <Row className="justify-content-center" >
-                                            <p>Si olvidaste tu usuario o contraseña cominucate con el área de admisitración.</p>
-                                        </Row>
-                                    </>
-                                )}
-                            </Formik>
-                        </Card>
-                    </Col>
-                </Row>
-            }
         </>
     );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,135 +6,134 @@ import { useNavigate } from "react-router-dom";
 
 const formSchema = Yup.object().shape({
   correo: Yup.string()
-    .required("Campo requerido"),  
+    .required("Campo requerido"),
   pass: Yup.string()
-  .required("Campo requerido"),
+    .required("Campo requerido"),
 });
 
-const URL = process.env.REACT_APP_BACKEND_CONNECTION; 
+const URL = process.env.REACT_APP_BACKEND_CONNECTION;
 
 function Login() {
-  const [usuario, setUsuario] = useState({legajo:'',correo:'', rol:'', pass: ''});
-  const [initialValues, setInitialValues] = useState({correo: '', pass: ''});
+  const [usuario, setUsuario] = useState({ legajo: '', correo: '', rol: '', pass: '' });
+  const [initialValues, setInitialValues] = useState({ correo: '', pass: '' });
 
   const navigate = useNavigate();
 
 
-  useEffect(() => {    
+  useEffect(() => {
     localStorage.clear();
   }, []);
-  
+
   const Ingresar = (values) => {
     const usuarioAux = {
       correo: values.correo,
       clave: values.pass,
-  };
-  
-  const options = {
+    };
+
+    const options = {
       method: 'POST',
       headers: {
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(usuarioAux),
-  };
+    };
 
-  console.log("Ingresar");
-  fetch(URL + 'api/Usuarios/Login', options)
+    console.log("Ingresar");
+    fetch(URL + 'api/Usuarios/Login', options)
       .then((res) => res.ok ? res.json() : Promise.reject(res))
       .then((data) => {
-          console.log(data);
-          localStorage.setItem("usuario",  JSON.stringify(data));
+        console.log(data);
+        setUsuario(
+          {
+            legajo: data.id,
+            correo: data.titulo,
+            rol: (data.tipoUsuario == 1) ? "Alumno" : (data.tipoUsuario == 2) ? "Profesor" : "Administrativo"
+          });
+
+        localStorage.setItem("usuario", JSON.stringify(data));
+        window.location.assign("/");
       })
       .catch((err) => {
-          console.error(err);
+        console.error('errror', err);
       })
       .finally(() => {
-        navigate("/");
       });
-    // let usuarioAux = usuarios.find(element => element.nombreUsuario === values.nombreUsuario && element.pass === values.pass );
-    // if(usuarioAux!= undefined){
-    //   localStorage.setItem("usuario",  JSON.stringify(usuarioAux));
-    //   navigate("/");
-    // }
-    // else{
-    //   navigate("/");
-    // }
   };
 
   return (
-        <Row className="justify-content-center">
-          <Col lg="8">
-            <Card style={{padding:10, color: "black"}} >
-              <Formik
-                initialValues={initialValues}
-                validationSchema={formSchema} 
-                onSubmit={Ingresar}>
-                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                <>
-                   <Row>
-                    <Col xs lg="12">
-                      <img  style={{height:"75px"}} alt="Logo PPSII Académico"/>
-                    </Col>                             
-                  </Row>
-                  <Row >
-                    <Col xs lg="12">
-                      <Form.Group controlId="correo">
-                        <Form.Label style={{ ...{color: "black"}}}>Usuario: </Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="correo"
-                          autoComplete='off'
-                          onChange={handleChange}
-                          value={values.correo}
-                          />
-                      </Form.Group>
-                      <ErrorMessage
-                            name='correo'
-                            component='div'
-                            className='field-error text-danger'
-                          /> 
-                    </Col>                             
-                  </Row>
-                  <Row >
-                    <Col xs lg="12">
-                      <Form.Group controlId="pass">
-                        <Form.Label style={{ ...{color: "black"}}}>Contraseña: </Form.Label>
-                        <Form.Control
-                          type="password"
-                          name="pass"
-                          autoComplete='off'
-                          onChange={handleChange}
-                          value={values.pass}
-                          />
-                      </Form.Group>
-                      <ErrorMessage
-                            name='pass'
-                            component='div'
-                            className='field-error text-danger'
-                          /> 
-                    </Col>                             
-                  </Row>
+    <Row className="justify-content-center m-2">
+      <Col sm='10' md='8' xl='6'>
+        <Card style={{ padding: 30, color: "black" }} >
+          <Formik
+            initialValues={initialValues}
+            validationSchema={formSchema}
+            onSubmit={Ingresar}>
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+              <>
+                <Row>
+                  <Col>
+                    <img src="https://iili.io/HIO1NSI.png" style={{ height: "75px" }} alt="Logo PPSII Académico" />
+                  </Col>
+                </Row>
+                <Row >
+                  <Col >
+                    <Form.Group controlId="correo">
+                      <Form.Label style={{ ...{ color: "black" } }}>E-mail: </Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="correo"
+                        autoComplete='off'
+                        onChange={handleChange}
+                        value={values.correo}
+                      />
+                    </Form.Group>
+                    <ErrorMessage
+                      name='correo'
+                      component='div'
+                      className='field-error text-danger'
+                    />
+                  </Col>
+                </Row>
+                <Row >
+                  <Col >
+                    <Form.Group controlId="pass">
+                      <Form.Label style={{ ...{ color: "black" } }}>Contraseña: </Form.Label>
+                      <Form.Control
+                        type="password"
+                        name="pass"
+                        autoComplete='off'
+                        onChange={handleChange}
+                        value={values.pass}
+                      />
+                    </Form.Group>
+                    <ErrorMessage
+                      name='pass'
+                      component='div'
+                      className='field-error text-danger'
+                    />
+                  </Col>
+                </Row>
 
-                  <Row className="justify-content-center">
-                    <Col xs lg="5">
-                      <Button
-                        variant="primary"
-                        onClick={handleSubmit}
-                        style={{width: "100%", background: "#FFFFFF", borderColor: "#009AAE", color: "#009AAE"}}
-                      >
-                        Ingresar hola
-                      </Button>
-                    </Col>          
-                  </Row>  
-                  <Row className="justify-content-center" >
-                    <p>Si olvidaste tu usuario o contraseña comunicate con el área de admisitración.</p>
-                  </Row>      
-                  </>
-                )}
-              </Formik>
-            </Card>            
-          </Col>
-        </Row>
+                <Row className="justify-content-center">
+                  <Col className="m-3" >
+                    <Button
+                      className="w-100"
+                      variant="outline-primary"
+                      onClick={handleSubmit}
+                    >
+                      Ingresar
+                    </Button>
+                  </Col>
+                </Row>
+                <Row className="justify-content-center" >
+                  <p>Si olvidaste tu usuario o contraseña cominucate con el área de admisitración.</p>
+                </Row>
+              </>
+            )}
+          </Formik>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
